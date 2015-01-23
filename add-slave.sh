4347db585039bceda018bd50ee8be2bac3100ef9
pablo rodriguez <prodriguezdefino@gmail.com>
2015-01-23 20:31:24 -0200

    changes in the slave mod script

--- add-slave.sh
+++ add-slave.sh
@@ -2,6 +2,14 @@
 
 echo "connected in $HOSTNAME"
 
+# will add an entry in hosts file
+
+echo "$1 $2" >> /etc/hosts
+echo "modified hosts file: "
+echo " "
+cat /etc/hosts
+echo " "
+echo "********************************"
 
 # will add a new slave node in the hadoop configuration.
 sed -i -e '$a\' /usr/local/hadoop/etc/hadoop/slaves
--- remote-add-slave.sh
+++ remote-add-slave.sh
@@ -3,6 +3,6 @@
 echo "Adding the machine $2 to the cluster as slave."
 
 ssh $1 bash -c "'
-/etc/add-slave.sh $2 
+/etc/add-slave.sh $2 $3 
 '"
 
